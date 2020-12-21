0a9abb9ed29913b673160fb693fa893c6b870ea5
Justin Nelson <justin.john.nelson@gmail.com>
2020-12-20 22:38:37 -0600

    lists raw text in files

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@
 // Import the module and reference it with the alias vscode in your code below
 import { TextEncoder } from 'util';
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as svelte from 'svelte/compiler';
+import { TemplateNode } from 'svelte/types/compiler/interfaces';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,6 +14,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "t-dot-svelte-vscode" is now active!');
 
+	vscode.window.registerTreeDataProvider(
+		't-dot-svelte-vscode.rawTextView',
+		new UntranslatedTextProvider()
+	);
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -19,7 +27,6 @@ export function activate(context: vscode.ExtensionContext) {
 			// The code you place here will be executed every time your command is executed
 
 			// Display a message box to the user
-
 			const editor = vscode.window.activeTextEditor;
 			if (!editor) return;
 
@@ -58,3 +65,80 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
+
+
+class UntranslatedTextProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+	constructor() { }
+
+	getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	async getChildren(element?: vscode.TreeItem): Promise<vscode.TreeItem[]> {
+		if (element instanceof SvelteFile) {
+			let source = (await vscode.workspace.openTextDocument(element.filePath)).getText();
+			let result: vscode.TreeItem[] = [];
+
+			try {
+				//let processed = await svelte.preprocess(source, svelte_preproces.typescript());
+				let parsed = svelte.parse(source);
+
+				return getRawText(parsed.html).map(text => new UntranslatedText(text.trim()));
+			} catch (e) {
+				console.log(e);
+				return [];
+			}
+		} else {
+			let svelteFiles = await vscode.workspace.findFiles("**/*.svelte");
+			let result: vscode.TreeItem[] = [];
+			for (let i = 0; i < svelteFiles.length; i++) {
+				let path = svelteFiles[i];
+				let source = (await vscode.workspace.openTextDocument(path)).getText();
+
+				try {
+					//let processed = await svelte.preprocess(source, svelte_preproces.typescript());
+					let parsed = svelte.parse(source);
+
+					if (templateHasRawText(parsed.html)) {
+						result.push(new SvelteFile(path, vscode.TreeItemCollapsibleState.Collapsed));
+					}
+				} catch (e) {
+					console.log(e);
+				}
+			}
+			return result;
+		};
+	}
+}
+
+function templateHasRawText(node: TemplateNode): boolean {
+	if (!node.children) return node.type == "Text" && node.raw.replace(/\s+/, '').length != 0;
+	return node.children.filter(nod => templateHasRawText(nod)).length != 0;
+}
+
+function getRawText(node: TemplateNode): string[] {
+	if (!node.children) return node.type == "Text" && node.raw.replace(/\s+/, '').length != 0 ? [node.raw] : [];
+	return node.children.map(nod => getRawText(nod)).reduce((prev, current) => prev.concat(current), []);
+}
+
+class SvelteFile extends vscode.TreeItem {
+	constructor(
+		public readonly filePath: vscode.Uri,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState
+	) {
+		super(path.basename(filePath.path), collapsibleState);
+		this.tooltip = filePath.path;
+		this.description = this.filePath.path;
+	}
+}
+
+class UntranslatedText extends vscode.TreeItem {
+	constructor(
+		public readonly rawText: string,
+	) {
+		super(rawText.substring(0, Math.min(15, rawText.length)) + (rawText.length < 15 ? '' : '...'), vscode.TreeItemCollapsibleState.None);
+		this.tooltip = rawText;
+		this.description = this.label + '';
+	}
+
+}
